5d505c26b715dc229138e5188e7061e1c5dd82dd
Victor Hugo Czinczik Moro <victor.czinczik@pucpr.edu.br>
2024-02-23 20:26:48 -0300

    victor

--- Aula_1_23-02/src/Main.java
+++ Aula_1_23-02/src/Main.java
@@ -1,8 +1,9 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        ATV1();
+        ATV3();
     }
 
     private  static void ATV1() {
@@ -15,4 +16,46 @@ public class Main {
         }
         scan.close();
     }
+
+
+    private static void ATV2(){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Digite um número: ");
+        int numero = scan.nextInt();
+        if (numero  > 0 ){
+            System.out.println("Seu número é positivo!");
+        } else if (numero == 0) {
+            System.out.println("Seu número é igual a 0!");
+        } else {
+            System.out.println("Seu número é negativo!");
+        }
+    }
+
+    private static void ATV3(){
+        Scanner scan = new Scanner(System.in);
+        double numeros[];
+        double soma = 0;
+        numeros = new double[3];
+        for (int i=0;i<3;i++){
+            System.out.println("Digite os números para calcular a média: ");
+            numeros[i] = scan.nextDouble();
+            soma += numeros[i];
+        }
+
+        double media = soma / numeros.length;
+        System.out.println("A média de " + Arrays.toString(numeros) + " é: " + media);
+
+    }
+
+    private static void ATV4(){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Digite sua idade: ");
+        int idade = scan.nextInt();
+        if (idade  > 18 ){
+            System.out.println("Você é maior de idade!");
+        } else if (idade < 18) {
+            System.out.println("Voce é menor de idade");
+        }
+    }
+
 }
