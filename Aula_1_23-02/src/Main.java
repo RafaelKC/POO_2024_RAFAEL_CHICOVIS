d9aeee6084619e4d98d9e53cee7fbfb40c150fc3
Luis Otavio Silva Santos <otavio.l@pucpr.edu.br>
2024-02-23 20:32:34 -0300

    exercicios 5, 6 e quase o 7

--- Aula_1_23-02/src/Main.java
+++ Aula_1_23-02/src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        ATV1();
+        ATV7();
     }
 
     private  static void ATV1() {
@@ -15,4 +15,58 @@ public class Main {
         }
         scan.close();
     }
+    private  static void ATV5() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println(" ");
+        System.out.println("=== Conversor de Celsius para Fahrenheit ===");
+        System.out.print("Digite o em Celsius: ");
+        int celsius = scan.nextInt();
+        int Fahrenheit = (int) ((celsius*1.8)+32);
+        System.out.println(celsius + "° Celsius equivale a " + Fahrenheit + "° Fahrenheit");
+
+        scan.close();
+    }
+
+    private  static void ATV6() {
+        System.out.println(" ");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("=== Dado D6 vitual ===");
+        System.out.print("Rodar dado?: ");
+        String resposta = scan.nextLine();
+
+        while (resposta.equals("sim") || resposta.equals("Sim")){
+            int int_random = (int)Math.floor(Math.random() * (6 - 1 + 1) + 1);
+            System.out.println("resultado: " + int_random);
+            System.out.println(" ");
+            System.out.print("Rodar dado novamente?: ");
+            resposta = scan.nextLine();
+
+        }
+
+    }
+
+    private  static void ATV7() {
+        System.out.println(" ");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("=== Pedra, Papel, tesoura ===");
+        System.out.println(" ");
+        System.out.println("1. Pedra ");
+        System.out.println("2. Papel");
+        System.out.println("3. Tesoura");
+        int resposta = scan.nextInt();
+
+        String[] opcoes = {"hahahahahahaha", "Pedra", "Papel", "Tesoura"};
+        int int_random = (int)Math.floor(Math.random() * (2 - 0 + 0) + 0);
+        System.out.println("Você: " + opcoes[resposta]);
+        System.out.println("Maquina: " + opcoes[int_random]);
+
+        if(opcoes[resposta].equals(opcoes[int_random])){
+            System.out.println("Empate!!");
+        } else if (resposta==1) {
+            
+        }
+
+
+    }
+
 }
